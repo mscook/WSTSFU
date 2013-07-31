a8eeaba514a4ccad232ed63db7576e0fcc79904e
Mitchell Stanton-Cook <mscook@gmail.com>
2013-07-31 17:57:40 +1000

    Boilerplate with limited functionality

--- WSTSFU/WSTSFU.py
+++ WSTSFU/WSTSFU.py
@@ -15,40 +15,41 @@
 
 
 """
-TODO: Single line description. 
-
-TODO: Detailed description
-
-### CHANGE LOG ### 
-YYYY-MM-DD <name> <address>
-    * Initial build
+Free clinical metadata relating to NGS experiments from Excel documents
 """
+
+
+
 import sys, os, traceback, argparse
 import time
 import __init__ as meta 
+import envoy
 
 epi = "Licence: "+meta.__licence__ +  " by " +meta.__author__ + " <" +meta.__author_email__ + ">"
 
-def main ():
+def to_CSV(file_path):
+    """
+    Convert the file to a csv
+    """
+    r = envoy.run('in2csv ' +file_path)
+    print r.std_out
 
-    global args
-    # TODO: Do something more interesting here...
-    print 'Hello world!'
-    # READ AND PRINT first positional argument (if any)
-    # If you want to access positional arguments with argparse:
-    # Use global "args" (declared above) not argv[0]
-    if args.output != None:
-        print 'Output: ' + args.output
-    print args
 
-    print args.verbose
-    print args.output
-    print args.arg1
+def core(args):
+    """
+    """
+    args.file = os.path.expanduser(args.file)
+    if not os.path.isfile(args.file):
+        print "Input file does not exit"
+        sys.exit(1)
+    else:
+        to_CSV(args.file) 
+    sys.exit(0)
 
 if __name__ == '__main__':
     try:
         start_time = time.time()
-        desc = __doc__.split('\n\n')[1].strip()
+        desc = __doc__.strip()
         parser = argparse.ArgumentParser(description=desc,epilog=epi)
         # EXAMPLE OF BOOLEAN FLAG: verbose
         parser.add_argument ('-v', '--verbose', action='store_true', default=False, help='verbose output')
@@ -56,15 +57,17 @@ if __name__ == '__main__':
         # EXAMPLE OF command line variable: 'output'
         parser.add_argument('-o','--output',action='store',help='output prefix')
         # EXAMPLE OF POSITIONAL ARGUMENT
-        parser.add_argument ('arg1', action='store', type=int, help='First positional argument (INT)')
-        parser.add_argument ('arg2', action='store', help='2nd positional argument (STRING)')
+        parser.add_argument ('file', action='store', type=str, 
+                                help='Full path to the metadata file')
+        #parser.add_argument ('arg2', action='store', help='2nd positional argument (STRING)')
         # EXAMPLE OF NESTED PARAMETERS
          
         # subparsers = parser.add_subparsers(help='commands')
         # list_parser = subparsers.add_parser('list', help='List contents')
         # list_parser.add_argument('dirname', action='store', help='Directory to list')
-
+        parser.set_defaults(func=core)
         args = parser.parse_args()
+        args.func(args)        
         if args.verbose: print "Executing @ " + time.asctime()
         main()
         if args.verbose: print "Ended @ " + time.asctime()
