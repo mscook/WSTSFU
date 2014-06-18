6db44f04c61c13ff3e6d2e630500fb479f89e034
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2014-06-18 13:52:40 +1000

    Reoriginisation - these no in src/WSTSFU/*

--- WSTSFU/__init__.py
+++
@@ -1,7 +0,0 @@
-__name__            = 'WSTSFU'
-__version__         = '0.1'
-__description__     = 'WorkSheet To Something Fucking Useful'
-__author__          = 'Mitchell Stanton-Cook'
-__author_email__    = 'm.stantoncook@gmail.com'
-__url__             = 'http://github.com/mscook/WSTSFU'
-__licence__         = 'ECL 2.0'
